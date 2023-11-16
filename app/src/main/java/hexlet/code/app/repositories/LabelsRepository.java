929f3aabf5a6cb9db72893e80ccfef9a31bd1153
Dmitrii Polkovnikov <dpolkovnikov@microsoft.com>
2023-11-16 12:33:27 +0100

    Add initialiazer for labels

--- app/src/main/java/hexlet/code/app/repositories/LabelsRepository.java
+++ app/src/main/java/hexlet/code/app/repositories/LabelsRepository.java
@@ -1,7 +1,11 @@
 package hexlet.code.app.repositories;
 
 import hexlet.code.app.models.Label;
+import hexlet.code.app.models.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LabelsRepository extends JpaRepository<Label, Long> {
+    Optional<Label> findByName(String name);
 }

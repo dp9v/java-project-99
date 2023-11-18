6defc58e9434ada9dcdb1439ae1cbc57fbb77cc9
dmitrii <polkovnikov13@gmail.com>
2023-11-18 13:52:15 +0100

    fix tests

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -4,6 +4,9 @@ import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long>,
-        JpaSpecificationExecutor<Task> {
+    JpaSpecificationExecutor<Task> {
+    Optional<Task> findByName(String name);
 }
